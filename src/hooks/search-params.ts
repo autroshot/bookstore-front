5fb4fd922754d354e334bac7df9c53575b2bd297
autroshot <autroshot@gmail.com>
2024-02-28 11:37:04 +0900

    useSearchParams 훅 구현

---
+++ src/hooks/search-params.ts
@@ -0,0 +1,87 @@
+import { useSearchParams as usePackageSearchParams } from 'react-router-dom';
+
+const QUERY_STRING_CONFIG = {
+    categoryId: { queryStringkey: 'category-id', type: 'number' },
+    isNew: { queryStringkey: 'is-new', type: 'boolean' },
+    view: { queryStringkey: 'view', type: 'string' },
+} as const satisfies QueryStringKeyMap;
+
+/**
+ * React Router의 useSearchParams에 key 제한과 형변환 기능을 추가한 훅이다.
+ */
+export default function useSearchParams() {
+    const [searchParams, setSearchParams] = usePackageSearchParams();
+
+    return { get, set, remove };
+
+    function get<T extends Name>(name: T): QueryStringKeyTypeWithNull[T] {
+        let result;
+
+        switch (getType(name)) {
+            case 'number': {
+                const value = searchParams.get(getQueryStringKey(name));
+                result = value === null ? null : Number(value);
+
+                break;
+            }
+            case 'boolean': {
+                const value = searchParams.get(getQueryStringKey(name));
+                result = value === null ? null : Boolean(value);
+
+                break;
+            }
+            case 'string': {
+                const value = searchParams.get(getQueryStringKey(name));
+                result = value;
+
+                break;
+            }
+            default: {
+                throw Error('QUERY_STRING_CONFIG에 존재하지 않는 키입니다.');
+            }
+        }
+
+        return result as QueryStringKeyTypeWithNull[T];
+    }
+
+    function set<T extends Name>(name: T, value: QueryStringKeyType[T]): void {
+        setSearchParams((urlSearchParams) => {
+            urlSearchParams.set(getQueryStringKey(name), String(value));
+            return urlSearchParams;
+        });
+    }
+
+    function remove(name: Name): void {
+        setSearchParams((urlSearchParams) => {
+            urlSearchParams.delete(getQueryStringKey(name));
+            return urlSearchParams;
+        });
+    }
+}
+
+function getQueryStringKey(name: Name) {
+    return QUERY_STRING_CONFIG[name]['queryStringkey'];
+}
+function getType(name: Name) {
+    return QUERY_STRING_CONFIG[name]['type'];
+}
+
+type QueryStringKeyMapLiteral = typeof QUERY_STRING_CONFIG;
+type QueryStringKeyType = {
+    [T in keyof QueryStringKeyMapLiteral]: QueryStringKeyMapLiteral[T]['type'] extends 'number'
+        ? number
+        : QueryStringKeyMapLiteral[T]['type'] extends 'boolean'
+        ? boolean
+        : QueryStringKeyMapLiteral[T]['type'] extends 'string'
+        ? string
+        : unknown;
+};
+type QueryStringKeyTypeWithNull = {
+    [T in keyof QueryStringKeyType]: QueryStringKeyType[T] | null;
+};
+type Name = keyof QueryStringKeyType;
+
+type LiteralTypeString = 'string' | 'number' | 'boolean';
+type QueryStringKeyMap = {
+    [key: string]: { queryStringkey: string; type: LiteralTypeString };
+};
